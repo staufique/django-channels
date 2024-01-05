ca41a3527b54aef9b5278c04826eb656f089b4a0
Mohammed Taufique Shaikh <59877720+staufique@users.noreply.github.com>
2024-01-05 12:27:53 +0530

    added path and routing for websocket

--- core/core/asgi.py
+++ core/core/asgi.py
@@ -11,6 +11,19 @@ import os
 
 from django.core.asgi import get_asgi_application
 
+#importing protocoltyperouter and urlrouter to handle websocket and hhtp request
+from channels.routing import ProtocolTypeRouter,URLRouter
+
+from django.urls import path
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_asgi_application()
+
+ws_patterns = [
+    path()
+]
+applications=ProtocolTypeRouter(
+    {
+        'websocket':URLRouter(ws_patterns)
+    }
+)
